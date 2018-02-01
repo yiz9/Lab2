29cda7a5138f1353fb96d93d237829ca2ead457c
18165 <yiz9@illinois.edu>
2018-01-31 18:51:14 -0600

    lab2

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,9 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            while(!maze.){
+
+            }// Implement your maze solving algorithm here
         }
 
         if (maze.isFinished()) {
